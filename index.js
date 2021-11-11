8e57808327639eaa2f7537af864e5e6c951f3552
kevin-liu <liu.kevin1997@gmail.com>
2021-11-11 18:30:22 -0500

    finished the assignment

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ class Student extends Lambdasian{
      return `${this.name} has submitted a PR for ${subject}`;
    }
    sprintChallenge(subject){
-     return `${Student.name} has begun sprint challenge on ${subject}`;
+     return `${this.name} has begun sprint challenge on ${subject}`;
    }
 }
 
@@ -205,7 +205,20 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor{
+  constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}){
+    super({name, age, location, specialty, favLanguage, catchPhrase});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`
+
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
+
    
 }
 /*
